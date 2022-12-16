5e956ba89e32c0e840a0bcf34386a57c8283fe43
Ari Brown <yabrown@princeton.edu>
2022-12-15 21:40:17 -0500

    swiftest cases

--- client/test_cases.go
+++ client/test_cases.go
@@ -6,6 +6,13 @@ import (
 	"math/rand"
 )
 
+func analyze(balancer *b.Balancer) {
+	fmt.Printf("Average Response Time: %.3fms\n", b.MeasureAverageResponseTime(balancer))
+	fmt.Printf("Average Task Load: %.3fms\n", b.MeasureAverageTaskLoad(balancer))
+	fmt.Printf("Average Task Time: %.3fms\n", b.MeasureAverageTaskTime(balancer))
+	b.MeasureLoadDistribution(balancer)
+}
+
 // servers: 5, each with 1 core
 // tasks: 1000, each random with load 1-100
 func const_cores_random_requests(alg string) {
@@ -50,10 +57,7 @@ func const_cores_random_requests(alg string) {
 	//fmt.Println("Sleeping for", timeToSleep)
 	<-end_signal
 	// system_info(servers, balancer)
-	fmt.Printf("Average Response Time: %.3fms\n", b.MeasureAverageResponseTime(balancer))
-	fmt.Printf("Average Task Load: %.3fms\n", b.MeasureAverageTaskLoad(balancer))
-	fmt.Printf("Average Task Time: %.3fms\n", b.MeasureAverageTaskTime(balancer))
-	b.MeasureLoadDistribution(balancer)
+	analyze(balancer)
 
 }
 
@@ -101,10 +105,7 @@ func const_cores_linear_requests(alg string) {
 	//fmt.Println("Sleeping for", timeToSleep)
 	<-end_signal
 	// system_info(servers, balancer)
-	fmt.Printf("Average Response Time: %.3fms\n", b.MeasureAverageResponseTime(balancer))
-	fmt.Printf("Average Task Load: %.3fms\n", b.MeasureAverageTaskLoad(balancer))
-	fmt.Printf("Average Task Time: %.3fms\n", b.MeasureAverageTaskTime(balancer))
-	b.MeasureLoadDistribution(balancer)
+	analyze(balancer)
 
 }
 
@@ -152,10 +153,7 @@ func const_cores_const_requests(alg string) {
 	//fmt.Println("Sleeping for", timeToSleep)
 	<-end_signal
 	// system_info(servers, balancer)
-	fmt.Printf("Average Response Time: %.3fms\n", b.MeasureAverageResponseTime(balancer))
-	fmt.Printf("Average Task Load: %.3fms\n", b.MeasureAverageTaskLoad(balancer))
-	fmt.Printf("Average Task Time: %.3fms\n", b.MeasureAverageTaskTime(balancer))
-	b.MeasureLoadDistribution(balancer)
+	analyze(balancer)
 
 }
 
@@ -204,10 +202,7 @@ func linear_cores_const_requests(alg string) {
 	//fmt.Println("Sleeping for", timeToSleep)
 	<-end_signal
 	// system_info(servers, balancer)
-	fmt.Printf("Average Response Time: %.3fms\n", b.MeasureAverageResponseTime(balancer))
-	fmt.Printf("Average Task Load: %.3fms\n", b.MeasureAverageTaskLoad(balancer))
-	fmt.Printf("Average Task Time: %.3fms\n", b.MeasureAverageTaskTime(balancer))
-	b.MeasureLoadDistribution(balancer)
+	analyze(balancer)
 
 }
 
@@ -256,10 +251,7 @@ func linear_cores_reverse_requests(alg string) {
 	//fmt.Println("Sleeping for", timeToSleep)
 	<-end_signal
 	// system_info(servers, balancer)
-	fmt.Printf("Average Response Time: %.3fms\n", b.MeasureAverageResponseTime(balancer))
-	fmt.Printf("Average Task Load: %.3fms\n", b.MeasureAverageTaskLoad(balancer))
-	fmt.Printf("Average Task Time: %.3fms\n", b.MeasureAverageTaskTime(balancer))
-	b.MeasureLoadDistribution(balancer)
+	analyze(balancer)
 
 }
 
@@ -307,9 +299,97 @@ func linear_cores_random_requests(alg string) {
 	//fmt.Println("Sleeping for", timeToSleep)
 	<-end_signal
 	// system_info(servers, balancer)
-	fmt.Printf("Average Response Time: %.3fms\n", b.MeasureAverageResponseTime(balancer))
-	fmt.Printf("Average Task Load: %.3fms\n", b.MeasureAverageTaskLoad(balancer))
-	fmt.Printf("Average Task Time: %.3fms\n", b.MeasureAverageTaskTime(balancer))
-	b.MeasureLoadDistribution(balancer)
+	analyze(balancer)
+}
 
+// servers: 5, each with 1 core
+// tasks: 1000, with best distribution-- even
+func diff_cores_same_requests(alg string, num_tasks int) {
+	//timeToSleep, _ := time.ParseDuration(os.Args[2] + "s")
+	// create 5 servers
+	s1 := b.NewServer(0, 1)
+	s2 := b.NewServer(1, 1)
+	s3 := b.NewServer(2, 3)
+	s4 := b.NewServer(3, 5)
+	s5 := b.NewServer(4, 10)
+	// create balancer
+	servers := []*b.Server{s1, s2, s3, s4, s5}
+	end_signal := make(chan bool)
+	balancer := b.NewBalancer(servers, end_signal)
+	if balancer == nil {
+	}
+	// create requests
+	requests := []*b.Request{}
+
+	for i := 0; i < num_tasks; i++ {
+		load := 10
+		requests = append(requests, b.NewRequest(i, load))
+	}
+	//wake all servers up
+	for i := range servers {
+		servers[i].Wake_up()
+	}
+	// assign each request
+	for i := range requests {
+		if alg == "robin" {
+			balancer.Assign_request_round_robin(requests[i])
+		} else if alg == "state" {
+			balancer.Assign_request(requests[i])
+		}
+	}
+	// system_info(servers, balancer)
+	//make each server active (start handling queued requests)
+	for i := range servers {
+		go servers[i].Work(balancer)
+	}
+	fmt.Println("Completing tasks with algorithm option", alg)
+	//fmt.Println("Sleeping for", timeToSleep)
+	<-end_signal
+	// system_info(servers, balancer)
+	analyze(balancer)
+}
+
+func diff_cores_diff_requests(alg string, num_tasks int) {
+	//timeToSleep, _ := time.ParseDuration(os.Args[2] + "s")
+	// create 5 servers
+	s1 := b.NewServer(0, 1)
+	s2 := b.NewServer(1, 1)
+	s3 := b.NewServer(2, 3)
+	s4 := b.NewServer(3, 5)
+	s5 := b.NewServer(4, 10)
+	// create balancer
+	servers := []*b.Server{s1, s2, s3, s4, s5}
+	end_signal := make(chan bool)
+	balancer := b.NewBalancer(servers, end_signal)
+	if balancer == nil {
+	}
+	// create requests
+	requests := []*b.Request{}
+
+	for i := 0; i < num_tasks; i++ {
+		random := (rand.Intn(20) + 1) * 10
+		requests = append(requests, b.NewRequest(i, random))
+	}
+	//wake all servers up
+	for i := range servers {
+		servers[i].Wake_up()
+	}
+	// assign each request
+	for i := range requests {
+		if alg == "robin" {
+			balancer.Assign_request_round_robin(requests[i])
+		} else if alg == "state" {
+			balancer.Assign_request(requests[i])
+		}
+	}
+	// system_info(servers, balancer)
+	//make each server active (start handling queued requests)
+	for i := range servers {
+		go servers[i].Work(balancer)
+	}
+	fmt.Println("Completing tasks with algorithm option", alg)
+	//fmt.Println("Sleeping for", timeToSleep)
+	<-end_signal
+	// system_info(servers, balancer)
+	analyze(balancer)
 }
